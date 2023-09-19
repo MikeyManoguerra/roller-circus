58c1291f4d20c812e3e84cd4eb98ff30617dc14a
mikeymanoguerra <mikeymanoguerra@gmail.com>
2023-09-19 16:41:22 -0400

    content improvements, image border

--- assets/scripts/bio-data.js
+++ assets/scripts/bio-data.js
@@ -1,34 +1,24 @@
 const bioData = (() => ({
   bios: [
     {
-      name: 'Rebel Rabbitt',
-      imageSrc: 'assets/images/rebel.jpg',
-      description: 'The clown bunny babe of Philadelphia. Whether they\'re hypnotizing you with their hips and hoops, shocking you with sideshow stunts, or teasing and tantalizing till you want to burst, this risqué rabbit is an unforgettable experience! They\'re the cottontail who raises hell, Rebel Rabbitt!'
-    },
-    {
-      name: 'Tipsy McTumble',
-      imageSrc: 'assets/images/tipsy.jpg',
-      description: 'The producer of Roller Circus and owner of Cotton Candy Circus Arts; tumbles her way into the hearts of audiences big and small. Tonight, this 3rd generation circus gal will dazzle you with her death-defying roller-disco aerial skills. '
-    },
-    {
-      name: 'Pizzle fo Shizzle',
-      imageSrc: 'assets/images/pizzle.jpg',
-      description: 'Master of the Rink; #302allday; quote “keep practicing until you make it look easy.”'
-    },
-    {
       name: 'AJ Kohn',
       imageSrc: 'assets/images/aj.jpg',
       description: 'Professional Freestyle Champion Skateboarder and owner of The Skateboard Academy of Philadelphia. AJ has been practicing the art of skateboarding for over 35 years. He\'s a real rad dude.'
     },
     {
-      name: 'Mikey Mangorilla',
-      imageSrc: 'assets/images/mikey.jpg',
-      description: 'Our next performer has big pants, big dreams and he likes trash. He can skate faster than a racoon running from a dumpster fire and juggle with his eyes open'
+      name: 'Emily Automaticbizooty',
+      imageSrc: 'assets/images/emily.jpg',
+      description: ''
     },
     {
       name: 'Margo Marz',
       imageSrc: 'assets/images/margo.jpg',
       description: ''
+    },
+    {
+      name: 'Mikey Mangorilla',
+      imageSrc: 'assets/images/mikey.jpg',
+      description: 'Our next performer has big pants, big dreams and he likes trash. He can skate faster than a racoon running from a dumpster fire and juggle with his eyes open'
     },
     {
       name: 'Miss Jay',
@@ -38,17 +28,27 @@ const bioData = (() => ({
     {
       name: 'Mo Sparks',
       imageSrc: 'assets/images/mo.jpg',
-      description: ''
-    },
-    {
-      name: 'Emily Automaticbizooty',
-      imageSrc: 'assets/images/emily.jpg',
-      description: ''
+      description: 'I’ve got a burning question: Are you ready for a roller disco inferno? This Rocksteady Roller is going to bring the heat. Be de-lighted by our very own human fire extinguisher… on wheels! MO SPARKS!'
     },
     {
       name: 'Pi',
       imageSrc: 'assets/images/pi.jpg',
-      description: ''
+      description: 'Pi is Philly’s little slice of silly and a shape-shifting HELLucination 👹🌀 They’re a costume designer, performer, makeup artist, and full-time idiot. Pi is a lipsync performer that’s inspired by inner-child whimsy, haunted houses, and raves. They are also a co-producer of @snatcherella3000 (the search for Philly’s next nightlife superstar)'
+    },
+    {
+      name: 'Pizzle fo Shizzle',
+      imageSrc: 'assets/images/pizzle.jpg',
+      description: 'Master of the Rink; #302allday; quote “keep practicing until you make it look easy.”'
+    },
+    {
+      name: 'Rebel Rabbitt',
+      imageSrc: 'assets/images/rebel.jpg',
+      description: 'The clown bunny babe of Philadelphia. Whether they\'re hypnotizing you with their hips and hoops, shocking you with sideshow stunts, or teasing and tantalizing till you want to burst, this risqué rabbit is an unforgettable experience! They\'re the cottontail who raises hell, Rebel Rabbitt!'
+    },
+    {
+      name: 'Tipsy McTumble',
+      imageSrc: 'assets/images/tipsy.jpg',
+      description: 'The producer of Roller Circus and owner of Cotton Candy Circus Arts; tumbles her way into the hearts of audiences big and small. Tonight, this 3rd generation circus gal will dazzle you with her death-defying roller-disco aerial skills. '
     },
   ]
 }))();
