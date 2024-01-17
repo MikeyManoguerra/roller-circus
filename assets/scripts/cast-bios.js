b8325e28653433feef85f62762bd7b2ffb63923a
mikeymanoguerra <mikeymanoguerra@gmail.com>
2024-01-17 13:56:53 -0500

    add wierdo and love city24 programs

--- assets/scripts/cast-bios.js
+++ assets/scripts/cast-bios.js
@@ -1,53 +1,69 @@
 const castBios = (() => ([
   {
+    slug: 'aj-kohn',
     name: 'AJ Kohn',
     imageSrc: 'assets/images/aj.jpg',
-    description: 'Professional Freestyle Champion Skateboarder and owner of The Skateboard Academy of Philadelphia, AJ has been practicing the art of skateboarding for over 35 years. He\'s a real rad dude.'
+    description: "Professional Freestyle Champion Skateboarder and owner of The Skateboard Academy of Philadelphia, AJ has been practicing the art of skateboarding for over 35 years. He's a real rad dude."
   },
   {
+    slug: 'emily-automaticbizooty',
     name: 'Emily Automaticbizooty',
     imageSrc: 'assets/images/emily.jpg',
     description: 'As this rock steady roller glides onto the stage, you will be captivated by her boundless energy and fun vibes, which will soon transform into awe at her astounding moves!'
   },
   {
+    slug: 'margo-marz',
     name: 'Margo Marz',
     imageSrc: 'assets/images/margo.jpg',
     description: 'She swoops down to the stage, and ecolocates that place in your mind to stimulate pure joy. Her endless tricks and stunts cannot be encircled easily!'
   },
   {
+    slug: 'mikey-mangorilla',
     name: 'Mikey Mangorilla',
     imageSrc: 'assets/images/mikey.jpg',
-    description: 'He wears someone else\'s oversized pants, He has big dreams and he likes trash. He juggles with his eyes open, and only flops when he means to.'
+    description: "He wears someone else's oversized pants, He has big dreams and he likes trash. He juggles with his eyes open, and only flops when he means to."
   },
   {
+    slug: 'ms-j',
     name: 'Ms J',
     imageSrc: 'assets/images/jay.jpg',
-    description: 'When not crafting custom clothing as J\'s Custom Vinyls, Ms J is on wheels, with a smile to light up the stage and with smooth moves and grooves to match!'
+    description: "When not crafting custom clothing as J's Custom Vinyls, Ms J is on wheels, with a smile to light up the stage and with smooth moves and grooves to match!"
   },
   {
+    slug: 'mo-sparks',
     name: 'Mo Sparks',
     imageSrc: 'assets/images/mo.jpg',
     description: 'She has a burning question: Are you ready for a roller disco inferno? This Rocksteady Roller is going to bring the heat. Be de-lighted by our very own human fire extinguisher… on wheels!'
   },
   {
+    slug: 'pi',
     name: 'Pi',
     imageSrc: 'assets/images/pi.jpg',
-    description: 'Pi is Philly\'s little slice of silly and a shape-shifting HELLucination 👹🌀 They\'re a costume designer, performer, makeup artist, and full-time idiot. Pi is a lipsync performer that’s inspired by inner-child whimsy, haunted houses, and raves.'
+    description: "Pi is Philly's little slice of silly and a shape-shifting HELLucination 👹🌀 They're a costume designer, performer, makeup artist, and full-time idiot. Pi is a lipsync performer that’s inspired by inner-child whimsy, haunted houses, and raves."
   },
   {
+    slug: 'pizzle-fo-shizzle',
     name: 'Pizzle fo Shizzle',
     imageSrc: 'assets/images/pizzle.jpg',
-    description: 'He\'s master of the Rink; #302allday. His wise words of advice: “keep practicing until you make it look easy.”'
+    description: "He's master of the Rink; #302allday. His wise words of advice: “keep practicing until you make it look easy.”"
   },
   {
+    slug: 'rebel-rabbitt',
     name: 'Rebel Rabbitt',
     imageSrc: 'assets/images/rebel.jpg',
-    description: 'This cottontail who raises hell is the clown bunny babe of Philadelphia. Whether they\'re hypnotizing you with their hips and hoops, shocking you with sideshow stunts, or teasing and tantalizing till you want to burst, this risqué rabbit is an unforgettable experience!'
+    description: "This cottontail who raises hell is the clown bunny babe of Philadelphia. Whether they're hypnotizing you with their hips and hoops, shocking you with sideshow stunts, or teasing and tantalizing till you want to burst, this risqué rabbit is an unforgettable experience!"
   },
   {
+    slug: 'tipsy-mctumble',
     name: 'Tipsy McTumble',
     imageSrc: 'assets/images/tipsy.jpg',
     description: 'The producer of Roller Circus and owner of Cotton Candy Circus Arts; tumbles her way into the hearts of audiences big and small. Tonight, this 3rd generation circus gal will dazzle you with her death-defying roller-disco aerial skills. '
+  },
+  {
+    slug: 'reese-crawley',
+    name: 'Reese Crawley',
+    imageSrc: 'assets/images/reese.jpg',
+    description: 'A Staple of the Philly fire spinning scene, Reese delights with skills across a variety of props, bringing technical skill and a glowing presence to the stage!'
   },
 ]
 ))();
--- assets/scripts/program.js
+++ assets/scripts/program.js
@@ -1,10 +1,21 @@
 /*global castBios*/
 
 // loop over the cast and create a context image component for each, append to dom
-castBios.forEach(({ name, imageSrc, description }) => {
-  const el = document.createElement('context-image')
-  el.setAttribute('data-name', name)
-  el.setAttribute('image-src', imageSrc)
-  el.setAttribute('description', description)
-  document.getElementById('program-bios').appendChild(el);
-});
+function build(slugs) {
+  slugs.forEach(slug => {
+    try {
+      const { name, imageSrc, description } = castBios.find(bio => bio.slug === slug);
+      const el = document.createElement('context-image')
+      el.setAttribute('data-name', name)
+      el.setAttribute('image-src', imageSrc)
+      el.setAttribute('description', description)
+      document.getElementById('program-bios').appendChild(el);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
+const program = (() => ({
+  build,
+}))();
